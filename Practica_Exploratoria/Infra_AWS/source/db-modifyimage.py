64606d411578a0f78759b00043f23e06b6598263
Arnau Colominas <acolominasilla@gmail.com>
2021-12-04 15:42:40 +0100

    lambda modify

--- Practica_Exploratoria/Infra_AWS/source/db-modifyimage.py
+++ Practica_Exploratoria/Infra_AWS/source/db-modifyimage.py
@@ -6,20 +6,30 @@ import os
 from boto3.dynamodb.conditions import Attr
 
 dynamodb = boto3.resource('dynamodb')
+s3 = boto3.client('s3')
+table_name = os.environ['TABLE_NAME']
+bucketS3 = os.environ['BUCKET_S3']
+region = os.environ['AWS_REGION']
 
-def store_image_s3(image,identifier):
-    extension = image['filename'].split('.')[1]
+
+def store_image_s3(image,filename,identifier):
+    extension = filename.split('.')[1]
     new_filename = identifier + '.' + extension
-    file_content = base64.b64decode(image['image_content'])
+    file_content = base64.b64decode(image)
     s3.put_object(Bucket=bucketS3, Key=new_filename, Body=file_content,ContentType='image/'+extension, ACL='public-read')
-    return new_filename
+    return new_filename, None
 
 def delete_image_s3(id):
-    image = table.scan(
-        FilterExpression=Attr('id').eq(value)
+    try:
+        res = table.scan(
+            FilterExpression=Attr('id').eq(id)
         )
-    filename = image['Items'][0]
-    resp = s3.delete_object(Bucket=bucketS3, Key=filename)
+        image = res['Items'][0]
+        filename = image['filename']
+        resp = s3.delete_object(Bucket=bucketS3, Key=filename)
+    except Exception as e:
+        return None, str(e)
+    return resp, None
 
 def store_image_dynamodb(id,title,description,keywords,author,creator,capture_date,storage_date,filename):
     table = dynamodb.Table(table_name)
@@ -37,12 +47,14 @@ def store_image_dynamodb(id,title,description,keywords,author,creator,capture_da
             'object_url': f'https://{bucketS3}.s3.{region}.amazonaws.com/{filename}'
         }
     )
+    return "OK", None
 
 def lambda_handler(event, context):
     image = event['image']
-    table_name = os.environ['TABLE_NAME']
 
-    for field in ["id","title", "description", "keywords","author","creator","capture_date","filename"]:
+    resp = "OK";
+
+    for field in ["id","title", "description", "keywords","author","creator","capture_date","storage_date","filename"]:
         if not event.get(field):
             return {
                 'status': 'fail',
@@ -55,14 +67,17 @@ def lambda_handler(event, context):
         keywords= event['keywords']
         author= event['author']
         creator = event['creator']
+        storage_date = event['storage_date']
         capture_date = event['capture_date']
         filename = event['filename']
 
     if event['image_content'] != "":
-        delete_image_s3(id)
-        filename = store_image_s3(image,id)
+        resp, msg = delete_image_s3(id)
+            if resp != None:
+                filename, msg = store_image_s3(image,filename,id)
 
-    store_image_dynamodb(id,title,description,keywords,author,creator,capture_date,storage_date,filename)
+    if resp!= None:
+        resp, msg = store_image_dynamodb(id,title,description,keywords,author,creator,capture_date,storage_date,filename)
 
     response = {
       'statusCode': 200,
